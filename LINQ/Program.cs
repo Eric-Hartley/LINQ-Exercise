e1a88b1961d0595364ad421a010948568c91d65d
Eric Hartley <spiffydude0@gmail.com>
2024-10-10 15:25:20 -0500

    completed exercise 2'

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var mlsTeams = new List<string>() { "Atlanta United FC", "Inter Miami FC", "Nashville SC", "Orlando City FC" };
+
+            var result = mlsTeams.OrderBy(x => x.Length);
+            foreach(var team in result)
+            {
+                Console.WriteLine(team);
+            }
+            
+            
+
         }
+
+        
+
+
     }
+    
 }
